35f6ae3e2aaf06dfa9307fad3dc33485ee22ff22
pehcy <cheeyung.peh@gmail.com>
2023-11-19 01:29:02 +0800

    Update and finalize gradio app

--- app.py
+++ app.py
@@ -7,8 +7,34 @@ import json
 import torch
 from torch import nn
 import numpy as np
-import locale
-locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+
+title = "# Malaysia KL House Price Prediction"
+
+description = """
+<br/>
+
+<img src="https://static.vecteezy.com/system/resources/previews/000/344/591/non_2x/kuala-lumpur-skyline-vector.jpg" style="width:200px;height:150px;">
+
+This machine learning model is to predict the house price 
+around the rural areas in Malaysia, Kuala Lumpur. 
+
+There are 11 types of houses in this dataset. 
+Apartment, Condominuim, Serviced Residence, Bungalow,
+Semi-detached House, and Terrace/Link Houses with different styles.
+
+Note that the [dataset](https://www.kaggle.com/datasets/dragonduck/property-listings-in-kuala-lumpur) that I used for training is a bit outdated (from 2019), 
+maybe you need to multiply the inflation rate to have more accurate figure.
+
+<br/>
+"""
+
+link = """
+|                                 |                                         |
+| ------------------------------- | --------------------------------------- |
+| 🏡📊 **KL House Price**        | <a style="display:inline-block" href='https://github.com/pehcy/Malaysia-House-Price-prediction'><img src='https://img.shields.io/github/stars/pehcy/Malaysia-House-Price-prediction?style=social' /></a>|
+"""
+
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 # Feed Forward Neural Network for tabular data
 class SparseTabularNN(nn.Module):
@@ -48,21 +74,8 @@ class SparseTabularNN(nn.Module):
 
 emb_size = [(77, 39), (9, 5), (6, 3), (9, 5), (85, 43), (3, 2)]
 model = SparseTabularNN(emb_size, 1, 1, [240, 70], dropout_rate=0.40)
-model.load_state_dict(torch.load('./HouseWeights.pt'))
-model.cuda()
-
-
-title = "Malaysia KL House Price Prediction"
-
-description = """
-
-<br/>
-
-<img src=""/>
-
-<br/>
-
-"""
+model.load_state_dict(torch.load('./HouseWeights.pt', map_location=torch.device(DEVICE)))
+model.to(DEVICE)
 
 locations_dict = None
 property_dict = None
@@ -104,8 +117,8 @@ def predict_price(
     else:
         property_index = property_dict[house_type]
 
-    X_cat = torch.tensor(np.array([[location_index,  rooms,  bathrooms, car_parks, property_index, furnished_status]]), dtype=torch.int64).to('cuda')
-    X_cont = torch.tensor(np.array([[size]]), dtype=torch.float32).reshape(-1, 1).to('cuda')
+    X_cat = torch.tensor(np.array([[location_index,  rooms,  bathrooms, car_parks, property_index, furnished_status]]), dtype=torch.int64).to(DEVICE)
+    X_cont = torch.tensor(np.array([[size]]), dtype=torch.float32).reshape(-1, 1).to(DEVICE)
 
     y_pred = 0
 
@@ -113,25 +126,26 @@ def predict_price(
         y_pred = model(X_cat, X_cont)
         y_pred = y_pred.item() * 1e6
         print(y_pred)
-        return f"""The expected house price is: MYR {locale.currency(y_pred, grouping=True)[1:]}"""
+        return f"""The expected house price is: MYR {y_pred:.2f}"""
 
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column():
-            rooms_gr = gr.Slider(0, 20, value=4, step=1.0, label="Number of Rooms", info="Choose between 0 and 20")
-            bathrooms_gr = gr.Slider(0, 20, value=4, step=1.0, label="Number of Bathrooms", info="Choose between 0 and 20")
-            car_parks_gr = gr.Slider(0, 20, value=4, step=1.0, label="Number of Car Parks", info="Choose between 0 and 20")
-            size_gr = gr.Slider(0, 10, value=1.5, step=0.01, label="Size (in 100,000 meter²)", info="Choose between 0 and 10")
-            furnished_gr = gr.Radio(["Unfurnished", "Partly Furnished", "Fully Furnished"], label="Furnished status", info="Is this house furnished?")
-                #gr.Slider(0, 20, value=4, step=1.0, label="Number of Bathrooms", info="Choose between 2 and 20"),
-            locations_gr = gr.Dropdown(
-                locations_dict.keys(), label="Location in KL", info="So far this is all locations we got!")
-        
+            gr.Markdown(title)
+            gr.Markdown(description)
         with gr.Column():
-            out = gr.Textbox()
-    
+            gr.Markdown(link)
+
     with gr.Row():
         with gr.Column():
+            rooms_gr = gr.Slider(0, 20, value=3, step=1.0, label="Number of Rooms", info="Choose between 0 and 20")
+            bathrooms_gr = gr.Slider(0, 20, value=1, step=1.0, label="Number of Bathrooms", info="Choose between 0 and 20")
+            car_parks_gr = gr.Slider(0, 20, value=1, step=1.0, label="Number of Car Parks", info="Choose between 0 and 20")
+            size_gr = gr.Slider(0, 10, value=1.5, step=0.01, label="Size (in 100,000 feet²)", info="Choose between 0 and 10")
+            furnished_gr = gr.Radio(["Unfurnished", "Partly Furnished", "Fully Furnished"], label="Furnished status", info="Is this house furnished?")
+            locations_gr = gr.Dropdown(
+                locations_dict.keys(), label="Location in KL", info="So far this is all locations we got!")
+            
             house_type_gr = gr.Dropdown([
                 "Apartment",
                 "Condominuim", 
@@ -144,35 +158,31 @@ with gr.Blocks() as demo:
                 "3-sty Terrace/Link House",
                 "3.5-sty Terrace/Link House",
                 "4-sty Terrace/Link House"
-            ], label="Furnished status", info="House Type")
+            ], label="House Type", info="Pick one of the house type")
 
             extra_rd = gr.Radio([
                 "Normal",
                 "Corner",
                 "Intermediate",
                 "EndLot"
-            ], label="Extra information for house property", info="Extra Information")
+            ], label="Extra information for house property", info="Choose one of the properties")
+
+            submit_btn = gr.Button("Submit", elem_id="send_tbn", visible=True)
         
         with gr.Column():
-            pass
-    
-    with gr.Row():
-        with gr.Column():
-            submit_btn = gr.Button("Send", elem_id="send_tbn", visible=True)
-            submit_btn.click(predict_price, [
-                rooms_gr, 
-                bathrooms_gr,
-                car_parks_gr, 
-                size_gr, 
-                furnished_gr, 
-                locations_gr,
-                house_type_gr,
-                extra_rd
-            ], 
-            outputs=out)
+            out = gr.Textbox(label="Prediction result")
         
-        with gr.Column():
-            pass
+    submit_btn.click(predict_price, [
+        rooms_gr, 
+        bathrooms_gr,
+        car_parks_gr, 
+        size_gr, 
+        furnished_gr, 
+        locations_gr,
+        house_type_gr,
+        extra_rd
+    ], 
+    outputs=out)
 
 if __name__ == "__main__":
     # start gradio app
